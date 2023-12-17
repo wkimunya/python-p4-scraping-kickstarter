1f9af50828b677ced17797473ed12cf75a311026
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-12-17 22:58:26 +0300

    completed lab

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,31 +1,26 @@
+# file: kickstarter_scraper.py
 from bs4 import BeautifulSoup
 import ipdb
-
 # projects: kickstarter.select("li.project.grid_4")[0]
 # title: project.select("h2.bbcard_name strong a")[0].text
 # image link: project.select("div.project-thumbnail a img")[0]['src']
 # description: project.select("p.bbcard_blurb")[0].text
 # location: project.select("ul.project-meta span.location-name")[0].text
 # percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-
 def create_project_dict():
-    html = ''
-    with open('./fixtures/kickstarter.html') as file:
+  html = ''
+  with open('./fixtures/kickstarter.html') as file:
         html = file.read()
-    kickstarter = BeautifulSoup(html, 'html.parser')
-    projects = {}
-    # Iterate through the projects
-    for project in kickstarter.select("li.project.grid_4"):
-        title = project.select("h2.bbcard_name strong a")[0].text
-        projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-        }
-    # return the projects dictionary
-
-    return projects
-
-projects = create_project_dict()
-print(projects)
+  kickstarter = BeautifulSoup(html, 'html.parser')
+  projects = {}
+  # Iterate through the projects
+  for project in kickstarter.select("li.project.grid_4"):
+    title = project.select("h2.bbcard_name strong a")[].text
+    projects[title] = {
+      'image_link': project.select("div.project-thumbnail a img").attribute("src").value,
+      'description': project.select("p.bbcard_blurb")[0].text,
+      'location': project.select("ul.project-meta span.location-name")[0].text,
+      'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")}
+  # return the projects dictionary
+  
+  return projects
